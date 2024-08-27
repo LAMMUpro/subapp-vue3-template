6b842f297e2797815b479e7b3fd41b7cce429883
陈永超 <chen_yongchao@126.com>
2024-08-27 18:41:21 +0800

    config: serveless部署

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import CONSTS from './src/utils/CONSTS'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import CONSTS from './src/utils/CONSTS';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: `/${CONSTS.PREFIX_URL}/`,
   resolve: {
     alias: {
       /** cdn位置 或 相对当前文件的位置 （本地运行时生效，打包external时不会替换关键词！！！） */
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  plugins: [vue()],
   server: {
     port: CONSTS.PORT,
     host: '0.0.0.0',
   },
-})
+  build: {
+    outDir: 'build',
+  },
+});
