83e140f9ba1d99d42e29e0c93bd5a00c2813fc8c
陈永超 <chen_yongchao@126.com>
2024-08-27 19:34:13 +0800

    demo: SvgIcon派发组件

--- src/pages/demo/micromainComponent.vue
+++ src/pages/demo/micromainComponent.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     vue3派发组件demo
+    <MicroComponent _is="SvgIcon" name="Github"></MicroComponent>
   </div>
 </template>
 
 <script lang="ts" setup>
-
+import MicroComponent from 'micro-app-utils/vue3/MicroComponent.vue';
 </script>
 
 <style lang="scss" scoped>
