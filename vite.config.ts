e075c86171f03b22d305a75577fc73d8ecdc68c8
LAMMU <chen_yongchao@126.com>
2024-10-06 11:22:13 +0800

    chore: 打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -19,53 +19,5 @@ export default defineConfig({
   },
   build: {
     outDir: 'build',
-    rollupOptions: {
-      plugins: [],
-      /** 打包排除这些依赖，保持import { xxx } from 'vue';这种导入 */
-      external: ['react', 'react-dom'],
-      output: {
-        format: 'esm', // 打包模式
-        /**
-         * 从对应网络路径中加载依赖
-         * 对于external排除的依赖，直接从'vue'导入是无效的路径，所以需要配置对应资源路径
-         */
-        paths: {
-          react: `/micromain/js/react.cd730000_h.js`,
-          'react-dom': `/micromain/js/react-dom.4bcc0000_h.js`,
-        },
-        /** 分包 */
-        manualChunks: {
-          // 'micro-app': ['@micro-zoe/micro-app'],
-        },
-        /** 入口文件输出格式 */
-        entryFileNames: 'js/[name].[hash]_h.js',
-        /** js文件输出格式 */
-        chunkFileNames: 'js/[name].[hash]_h.js',
-        /** 资源文件输出格式 */
-        assetFileNames: (assetInfo) => {
-          /** 默认输出格式 */
-          const defaultFormat = '[ext]/[name].[hash]_h.[ext]';
-          /** 图片后缀 */
-          const imageExts = ['png', 'jpg', 'jpeg', 'webp', 'gif', 'svg', 'ico'];
-          /** 字体后缀 */
-          const fontExts = ['ttf', 'woff', 'woff2'];
-          /** 当前文件后缀 */
-          const fileExt = assetInfo.name?.split('.').slice(-1)[0];
-          /** 提取文件后缀异常处理 */
-          if (!fileExt) {
-            console.warn('>>> 文件后缀异常: ', assetInfo);
-            return defaultFormat;
-          }
-          /** 图片资源 */
-          if (imageExts.includes(fileExt)) {
-            return 'img/[name].[hash]_h.[ext]';
-          } else if (fontExts.includes(fileExt)) {
-            return 'font/[name].[hash]_h.[ext]';
-          }
-          /** 其它资源 */
-          return defaultFormat;
-        },
-      },
-    },
   },
 });
