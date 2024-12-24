f35e6c2de81f38b656c8980dec24b5e4b20aab75
陈永超 <chen_yongchao@126.com>
2024-12-24 16:47:55 +0800

    demo: frame-less-ui组件

--- src/router/index.ts
+++ src/router/index.ts
@@ -49,6 +49,12 @@ const demoRoutes: Array<RouteRecordRaw> = [
     meta: {},
   },
   {
+    path: '/demo/frame-less-ui',
+    name: 'frameLessUI',
+    component: () => import('@/pages/demo/frameLessUI.vue'),
+    meta: { title: 'frame-less-ui组件库测试' },
+  },
+  {
     path: `/home`,
     name: `home`,
     component: () => import('@/pages/home.vue'),
